ea3990b27f7121d56e56666359482a72eeb0229f
Mario Sanjuan Canto <msancan3@upv.edu.es>
2026-02-25 12:29:52 +0100

    fix: minor changes

--- ProjectCode/Backend/agents/base_agent.py
+++ ProjectCode/Backend/agents/base_agent.py
@@ -8,11 +8,17 @@ class BaseAgent:
     def __init__(self, name, memory):
         self.name = name
         self.memory = memory
-        self.client = OpenAI(api_key = os.getenv("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"), base_url=os.getenv("https://openrouter.ai/api/v1"))
+        self.client = OpenAI(api_key = os.getenv("OPENROUTER_API_KEY"), base_url=os.getenv("OPENROUTER_URL"))
 
 
-    def generate(self, prompt, model="openrouter/free"):
-        response = 
+    def _call_llm(self, prompt, model="openrouter/free"):
+        response = self.client.chat.completions.create(
+            model = model,
+            max_tokens = 2000,
+            messages = prompt
+        )
+
+        return response.choices[0].message.content
 
 
 
