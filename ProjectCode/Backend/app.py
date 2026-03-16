e95c5b81e2dfab709232873f8f008e0184ce7309
Mario Sanjuan Canto <msancan3@upv.edu.es>
2026-03-16 10:13:58 +0100

    fix: minor changes

--- ProjectCode/Backend/app.py
+++ ProjectCode/Backend/app.py
@@ -8,42 +8,6 @@ from services.exercise_service import ExerciseService
 app = Flask(__name__)
 CORS(app)
 
-'''
-@app.route('/api/generate_exercise', methods=['POST'])
-def generate_exercise_endpoint():
-    """
-    Endpoint to generate cognitive exercises from memory data.
-    """
-    print("[app] generate_exercise_endpoint")
-
-    # --- Request Input validation ---
-    if not request.is_json:
-        return jsonify({"error": "Request must be JSON"}), 400
-
-    memory_data = request.get_json()
-    user_id = memory_data.get('user_id')
-
-    if not memory_data or 'title' not in memory_data or 'user_description' not in memory_data:
-        return jsonify({
-            "error": "JSON must contain 'title' and 'user_description'."
-        }), 400
-
-    # --- Exercise generation logic ---
-    
-    # We now generate the full set of exercises (3 different types)
-    strategy = logic.determine_next_exercise_strategy(user_id)
-    
-    exercise_set = logic.generate_cognitive_exercises(
-        memory_data, 
-        strategy,
-        user_id
-    )
-    
-    if not exercise_set or not exercise_set.get("exercises"):
-        return jsonify({"error": "Could not generate exercises."}), 500
-
-    return jsonify(exercise_set)
-'''
 @app.route('/api/excercise_correction', methods=['POST'])
 def excercise_correction_endpoint():
     """
@@ -80,6 +44,7 @@ def excercise_correction_endpoint():
     except Exception as e:
         print(f"[app] Error updating user stats: {e}")
         return jsonify({"error": "Could not update user stats."}), 500
+
 
 @app.route('/api/test', methods=['GET'])
 def test_endpoint():
